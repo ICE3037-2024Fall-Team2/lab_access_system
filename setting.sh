334e2386fe7a38af961af1cb8cde2599177c2077
웨이칭 <wyq1102@g.skku.edu>
2024-12-12 19:45:32 +0900

    [Debug] accidently closed

--- setting.sh
+++ setting.sh
@@ -7,8 +7,14 @@ MAIN_APP_PATH="filelamlai/main.py"
 echo "Activating virtual environment and starting Flask..."
 source "$VENV_PATH/bin/activate"
 nohup python3 "$FLASK_APP_PATH" > flask.log 2>&1 &  
+FLASK_PID=$!  
+echo "Flask started with PID $FLASK_PID"
 
 deactivate
 
 echo "Running main.py..."
 python3 "$MAIN_APP_PATH"
+
+echo "Stopping Flask with PID $FLASK_PID..."
+kill $FLASK_PID
+
